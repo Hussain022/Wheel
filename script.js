74bb59e3a56d17bdea647d005d06398a30781b32
Hussain022 <111119731+Hussain022@users.noreply.github.com>
2024-03-10 17:06:41 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+const userInput = () => {
+    let input = document.querySelector("#number").value;
+    alert('The speed is: ' + input + ' cicle per second');
+    document.querySelector(".wheel").style.animationDuration = input+'s';
+    let n = document.querySelectorAll(".cabin");
+    console.log(n);
+
+    for(i=0; n.length; i++){
+        n[i].style.animationDuration = (input)+'s';
+    }
+    
+    
+} 
