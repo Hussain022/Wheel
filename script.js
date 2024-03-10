d2817de752d8714531e129f78ca2ef67761c98c7
Hussain022 <111119731+Hussain022@users.noreply.github.com>
2024-03-10 17:38:18 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const userInput = () => {
     let input = document.querySelector("#number").value;
     alert('The speed is: ' + input + ' cicle per second');
-    document.querySelector(".wheel").style.animationDuration = input+'s';
+    document.querySelector(".wheel").style.animationDuration = (1/input)+'s';
     let n = document.querySelectorAll(".cabin");
     console.log(n);
 
     for(i=0; n.length; i++){
-        n[i].style.animationDuration = (input)+'s';
+        n[i].style.animationDuration = (1/input)+'s';
     }
     
     
